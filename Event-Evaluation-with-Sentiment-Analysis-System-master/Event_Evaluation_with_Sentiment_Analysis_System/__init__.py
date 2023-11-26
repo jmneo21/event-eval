dbe97955384657db80e2b9b7d374fc8df767d3a2
jmneo21 <125106579+jmneo21@users.noreply.github.com>
2023-11-26 11:33:31 +0800

    Add files via upload

---
+++ Event-Evaluation-with-Sentiment-Analysis-System-master/Event_Evaluation_with_Sentiment_Analysis_System/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+app = Flask(__name__)
+
+import Event_Evaluation_with_Sentiment_Analysis_System.views
---
+++ Event-Evaluation-with-Sentiment-Analysis-System-master/runserver.py
@@ -0,0 +1,5 @@
+from os import environ
+from Event_Evaluation_with_Sentiment_Analysis_System import app
+
+if __name__ == '__main__':
+     app.run(host='localhost', port=5555)

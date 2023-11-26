655ddc154af229cba208ed0e1274b38f154d7faa
jmneo21 <125106579+jmneo21@users.noreply.github.com>
2023-11-26 14:31:05 +0800

    Delete Event-Evaluation-with-Sentiment-Analysis-System-master directory

--- Event-Evaluation-with-Sentiment-Analysis-System-master/Event_Evaluation_with_Sentiment_Analysis_System/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-import Event_Evaluation_with_Sentiment_Analysis_System.views
--- Event-Evaluation-with-Sentiment-Analysis-System-master/runserver.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-from Event_Evaluation_with_Sentiment_Analysis_System import app
-
-if __name__ == '__main__':
-     app.run(host='localhost', port=5555)
